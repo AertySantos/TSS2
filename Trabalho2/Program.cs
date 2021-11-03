8329c515d72a64b74908567879805e4b0d5f866a
aertyps <aertyps@hotmail.com>
2021-11-03 01:00:43 -0300

    dsadasdas

--- Trabalho2/Program.cs
+++ Trabalho2/Program.cs
@@ -6,7 +6,8 @@ namespace Trabalho2
     {
         static void Main(string[] args)
         {
-            Elevador elevador = new Elevador( 5, false, 0);
+
+            //Elevador elevador = new Elevador( 5, false, 0);
             Console.WriteLine("Ola mundo2!");
         }
     }
