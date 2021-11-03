299fd7df4b36e4d3028b2e2242e79ab1fdcb4cf4
aertyps <aerty@ucl.br>
2021-11-02 21:00:45 -0300

    classe botao

---
+++ Trabalho2/Botao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabalho2
+{
+    class Botao
+    {
+        private bool acao;
+        private bool estado;
+        public Botao()
+        {
+
+        }
+
+        public void SetAcao(bool a)
+        {
+            this.acao = a;
+        }
+
+        public void SetEstado(bool e)
+        {
+            this.estado = e;
+        }
+
+        public bool GetAcao()
+        {
+            return this.acao;
+        }
+
+        public bool GetEstado()
+        {
+            return this.estado;
+        }
+    }
+}
---
+++ Trabalho2/Elevador.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabalho2
+{
+    class Elevador
+    {
+    }
+}
